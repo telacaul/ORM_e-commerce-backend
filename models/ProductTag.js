36b7c95cd621153f594cc33c16c9b94c859e4fcc
Tela Caul <telacaul@gmail.com>
2021-05-02 20:26:56 -0700

    product tags defined

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,30 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // defining id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    
+    },
+    //defining product_id column
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id"
+      }
+    },
+    //defining tag_id column
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
